624eb22069aa987942d655848f9c24eed531f2d1
Karim Naqvi <naqvik@uregina.ca>
2021-09-15 14:05:17 -0600

    Started a great sonnet

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+
+std::string f() {
+    using namespace std;
+    string s =
+        "Shall I compare thee to a summer's day?\n"
+        "Thou art more lovely and more temperate\n"
+        ;
+}
+
 int main( ) {
-    std::cout << "Hello\n";
+    using namespace std;
+    cout << "Hello\n";
+
+    cout << "Here's my idea for a poem!  What do you think?\n\n";
+    
 }
